b1b746e260a1fc61d07bae7de259ac6d1c24b712
Rishit Khare <kharerishit@gmail.com>
2022-02-22 15:16:56 -0600

    Indent fixes

--- scripts/Dream.gd
+++ scripts/Dream.gd
@@ -9,11 +9,11 @@ export(String, FILE) var move_to
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 func _input(event):
-    if Input.is_action_just_pressed("wake_up"):
-        GameManager.goto_scene(move_to)
+	if Input.is_action_just_pressed("wake_up"):
+		GameManager.goto_scene(move_to)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -10,32 +10,32 @@ var current_scene = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    var root = get_tree().get_root()
-    current_scene = root.get_child(root.get_child_count() - 1)
-    
-    SignalBus.connect("item_picked_up", self, "on_item_picked_up")
-    
+	var root = get_tree().get_root()
+	current_scene = root.get_child(root.get_child_count() - 1)
+	
+	SignalBus.connect("item_picked_up", self, "on_item_picked_up")
+	
 
 func on_item_picked_up(type):
-    inventory_flags[type] = true
-    print(inventory_flags)
+	inventory_flags[type] = true
+	print(inventory_flags)
 
 func goto_scene(path):
-    call_deferred("_deferred_goto_scene", path)
+	call_deferred("_deferred_goto_scene", path)
 
 
 func _deferred_goto_scene(path):
-    # It is now safe to remove the current scene
-    current_scene.free()
+	# It is now safe to remove the current scene
+	current_scene.free()
 
-    # Load the new scene.
-    var s = ResourceLoader.load(path)
+	# Load the new scene.
+	var s = ResourceLoader.load(path)
 
-    # Instance the new scene.
-    current_scene = s.instance()
+	# Instance the new scene.
+	current_scene = s.instance()
 
-    # Add it to the active scene, as child of root.
-    get_tree().get_root().add_child(current_scene)
+	# Add it to the active scene, as child of root.
+	get_tree().get_root().add_child(current_scene)
 
-    # Optionally, to make it compatible with the SceneTree.change_scene() API.
-    get_tree().set_current_scene(current_scene)
+	# Optionally, to make it compatible with the SceneTree.change_scene() API.
+	get_tree().set_current_scene(current_scene)
