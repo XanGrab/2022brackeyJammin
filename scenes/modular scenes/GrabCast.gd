bbf30c96a76ec89354d66d62ee3f74966a630a93
Rishit Khare <kharerishit@gmail.com>
2022-02-24 20:30:03 -0600

    push/pull ability only active when bracelet collected

--- scenes/modular scenes/GrabCast.gd
+++ scenes/modular scenes/GrabCast.gd
@@ -3,9 +3,15 @@ extends RayCast2D
 # script that attaches to player and interacts with other objects
 const Interactable = preload("res://scripts/Interactable.gd")
 
-const RAY_LENGTH = 12
+const RAY_LENGTH = 9
+# const 
 
 onready var player = $".." # gets reference to player_movement.gd
+
+func _ready():
+	enabled = false
+	SignalBus.connect("item_picked_up", self, "_on_item_collection")
+	
 
 func _physics_process(_delta):
 	cast_to = player.direction * RAY_LENGTH
@@ -18,7 +24,15 @@ func _physics_process(_delta):
 func _input(event):
 	if(Input.is_action_pressed("grab") && is_colliding()):
 		var collider = get_collider()
-		print(collider)
 			
 		if(collider.is_in_group("Pushable")):
 			player.start_pushing(collider)
+			
+func update_grab_ability():
+	if(GameManager.inventory_flags[GameManager.Item.BRACELET]):
+		enabled = true
+	else:
+		enabled = false
+
+func _on_item_collection(item):
+	update_grab_ability()
